17e9a2bb2dff8aa4b4c060a3e0b679e993b8f4b0
kiki <kiki@kikinoiMac.local>
2018-07-18 23:55:28 +0900

    [add]スペクトルを扱うクラスを追加

---
+++ Spectrum.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kiki on 2018/07/18.
+//
+
+#include "Spectrum.hpp"
---
+++ Spectrum.hpp
@@ -0,0 +1,14 @@
+//
+// Created by kiki on 2018/07/18.
+//
+
+#ifndef PATHTRACING_SPECTRUM_HPP
+#define PATHTRACING_SPECTRUM_HPP
+
+namespace nagato {
+class Spectrum {
+
+};
+}
+
+#endif //PATHTRACING_SPECTRUM_HPP
