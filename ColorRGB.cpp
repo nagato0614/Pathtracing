a6b2f45898b318cd32e5ece773217a3bb6311661
kiki <kiki@kikinoiMac.local>
2018-07-19 23:45:36 +0900

    [add]RGBを扱うクラスを追加

---
+++ ColorRGB.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kiki on 2018/07/19.
+//
+
+#include "ColorRGB.hpp"
---
+++ ColorRGB.hpp
@@ -0,0 +1,15 @@
+//
+// Created by kiki on 2018/07/19.
+//
+
+#ifndef PATHTRACING_COLORRGB_HPP
+#define PATHTRACING_COLORRGB_HPP
+
+
+class ColorRGB
+{
+
+};
+
+
+#endif //PATHTRACING_COLORRGB_HPP
