7aa0495984d181e6a0259baf7da9534381747165
kiki <kiki@kikinoiMac.local>
2018-07-21 01:06:57 +0900

    [clean]ColorRGB::toRGB()の処理の記述の仕方を変更

--- ColorRGB.cpp
+++ ColorRGB.cpp
@@ -19,20 +19,20 @@ namespace nagato
 
     void ColorRGB::spectrum2rgb(Spectrum s, Spectrum red, Spectrum green, Spectrum blue)
     {
-        const auto spectrumX = red * s;
-        const auto spectrumY = green * s;
-        const auto spectrumZ = blue * s;
+        auto k = 1.0 / red.sum();
+        const auto spectrumX = (red * s) * k;
+        const auto spectrumY = (green * s) * k;
+        const auto spectrumZ = (blue * s) * k;
 
         ColorRGB buff(0);
-        for (int i = 0; i < s.resolution_ + 1; ++i) {
-            buff.r += spectrumX.spectrum[i];
-            buff.g += spectrumY.spectrum[i];
-            buff.b += spectrumZ.spectrum[i];
-        }
+        buff.r = spectrumX.sum();
+        buff.g = spectrumY.sum();
+        buff.b = spectrumZ.sum();
 
-        double torgb[3][3] = {{3.2404542, -1.5371385, -0.4985314},
-                              {-0.9692660, 1.8760108, 0.0415560},
-                              {0.0556434, -0.2040259, 1.0572252},};
+        // XYZ色相系からRGBに変換する行列
+        const double torgb[3][3] = {{3.2404542,  -1.5371385, -0.4985314},
+                                    {-0.9692660, 1.8760108,  0.0415560},
+                                    {0.0556434,  -0.2040259, 1.0572252},};
         r = buff.r * torgb[0][0] + buff.g * torgb[0][1] + buff.b * torgb[0][2];
         g = buff.r * torgb[1][0] + buff.g * torgb[1][1] + buff.b * torgb[1][2];
         b = buff.r * torgb[2][0] + buff.g * torgb[2][1] + buff.b * torgb[2][2];
