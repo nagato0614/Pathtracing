75aec48c7a9e99d482dce7a2769242923dbc6f47
nagai toru <nagato0614@gmail.com>
2018-07-23 22:49:39 +0900

    [add]Materialの設定を記述するクラスMaterialを追加

---
+++ src/Material.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 長井亨 on 2018/07/23.
+//
+
+#include "Material.hpp"
---
+++ src/Material.hpp
@@ -0,0 +1,17 @@
+//
+// Created by 長井亨 on 2018/07/23.
+//
+
+#ifndef PATHTRACING_MATERIAL_HPP
+#define PATHTRACING_MATERIAL_HPP
+
+namespace nagato
+{
+
+    class Material
+    {
+
+    };
+
+}
+#endif //PATHTRACING_MATERIAL_HPP
