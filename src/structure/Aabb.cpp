c1c1cf20a1191a3eacf460c59382b0ca7ed35bfa
nagato0614 <nagato0614@gmail.com>
2018-09-07 01:16:32 +0900

    [update]AABBとrayの当たり判定を修正

--- src/structure/Aabb.cpp
+++ src/structure/Aabb.cpp
@@ -2,6 +2,7 @@
 // Created by 長井亨 on 2018/07/28.
 //
 
+#include <float.h>
 #include "Aabb.hpp"
 namespace nagato{
     Aabb::Aabb(Vector3 min, Vector3 max) : min(min), max(max)
@@ -11,18 +12,24 @@ namespace nagato{
 
     bool Aabb::intersect(Ray &ray)
     {
-        int h = 2;
-        for (int v = 0; v < 3; v++) {
-            if (min[v] * ray.direction[h] > max[h] * ray.direction[v] ||
-                    min[h] * ray.direction[v] > max[v] * ray.direction[h])
-                return false;
+        float t_max =  FLT_MAX ; // AABB からレイが外に出る時刻
+        float t_min = -FLT_MAX;  // AABB にレイが侵入する時刻
 
-            h = v;
+        for (int i=0; i<3; i++) {
+            float t1 = (min[i] - ray.origin[i])/ray.direction[i];
+            float t2 = (max[i] - ray.origin[i])/ray.direction[i];
+            float t_near = std::min(t1, t2);
+            float t_far = std::max(t1, t2);
+            t_max = std::min(t_max, t_far);
+            t_min = std::max(t_min, t_near);
+
+            // レイが外に出る時刻と侵入する時刻が逆転している => 交差していない
+            if (t_min > t_max) return false;
         }
         return true;
     }
 
-    Aabb::Aabb() : min(Vector3(MAXFLOAT)), max(Vector3(-MAXFLOAT))
+    Aabb::Aabb() : min(Vector3(1e+1000)), max(Vector3(static_cast<float>(-1e+1000)))
     {
 
     }
